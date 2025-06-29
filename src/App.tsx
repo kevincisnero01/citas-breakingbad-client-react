66d96470e8a580b002cc0a4b64e3bc15d6f0c561
Kevin Cisnero <kevingabriel18@gmail.com>
2025-06-28 22:23:52 -0400

    feat(api): Project Base Archives

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import styled from '@emotion/styled';
+
+const Contenedor = styled.div`
+display: flex;
+align-items: center;
+padding-top: 5rem;
+flex-direction: column;
+`;
+
+const Boton = styled.button`
+background: -webkit-linear-gradient(top left, #007d35 0%, #117d35 40%, #0f574e 100%);
+background-size 300px;
+font-family: Arial, Helvetica, sans-serif;
+color: #fff;
+maring-top: 3rem;
+padding: 1rem 3rem;
+font-size: 2rem;
+border: 2px solid black;
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const consultarAPI = () => {
+    console.log('consultando...');
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Contenedor>
+      <Boton
+        onClick={consultarAPI}
+      >
+        Obtener Frase
+      </Boton>
+    </Contenedor>
   )
 }
 
